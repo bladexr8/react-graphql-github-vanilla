9fbe6080f1dcb551b2cbf8d0546de988c59bf2f5
Stephen Moss <bladexr8@gmail.com>
2022-04-26 20:08:24 +1000

    initial GraphQL Query

--- src/App.js
+++ src/App.js
@@ -8,12 +8,54 @@ const axiosGitHubGraphQL = axios.create({
   }
 });
 
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`
+
 const TITLE = 'React GraphQL GitHub Client';
 
 class App extends Component {
 
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+    organization: null,
+    errors: null,
+  };
+
+  componentDidMount() {
+    this.onFetchFromGitHub();
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value});
+  }
+
+  onSubmit = event => {
+    // fetch data
+  }
+
+  onFetchFromGitHub = () => {
+    axiosGitHubGraphQL
+      .post('', { query: GET_ORGANIZATION})
+      .then(result => {
+        console.log(result);
+        this.setState(() => ({
+          organization: result.data.data.organization,
+          errors: result.data.errors,
+        }))
+      });
+  }
+
   render() {
-    console.log(`Access Token = ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`)
+    //console.log(`Access Token = ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`)
+
+    const { path, organization, errors } = this.state;
+
     return (
       <div>
         <h1>{TITLE}</h1>
@@ -25,6 +67,7 @@ class App extends Component {
           <input
             id="url"
             type="text"
+            value={path}
             onChange={this.onChange}
             style={{ width: '300px' }}
           />
@@ -35,9 +78,35 @@ class App extends Component {
 
         {/* Here comes the result... */}
 
+        {organization ? (
+          <Organization organization={organization} errors={errors} />
+        ) : (
+          <p>No information yet...</p>
+        )}
+        
       </div>
     );
   }
+}
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+
+  return (
+  <div>
+    <p>
+      <strong>Issues from Organization:</strong>
+      <a href={organization.url}>{organization.name}</a>
+    </p>
+  </div>
+  );
 }
 
 export default App;
